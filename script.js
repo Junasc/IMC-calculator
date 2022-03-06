e4ade97e8d27663950b9a81dfec08d3610a14818
Juliana Nascimento <pessoal.juliana.n@gmail.com>
2022-03-06 16:08:10 -0300

    minor bugs fixes

--- script.js
+++ script.js
@@ -2,26 +2,32 @@ const calculato = document.getElementById('calculato');
 
 function imc()
 {
-    const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const result = document.getElementById('result');
 
-    var resultImc = peso / (altura * altura );
-    var showResult = ' ';
+    var resultImc = peso / (altura * altura);
+    var showResult = '';
+
+    if(Number(peso) < 0 || Number(altura) < 0)
+    {
+       return result.innerText = `insira um valor positivo!`;
+
+    }
+
     if(resultImc < 18)
     {
         showResult = 'Magreza';
     }
-    if(resultImc > 18,5 && resultImc < 24,9)
+    if(resultImc > 18.5 && resultImc < 24.9)
     {
         showResult = 'Normal';
     }
-    if(resultImc > 25 && resultImc < 29,9)
+    if(resultImc > 25 && resultImc < 29.9)
     {
         showResult = 'Sobrepeso, grau de obesidade I';
     }
-    if(resultImc > 30 && resultImc < 39,9)
+    if(resultImc > 30 && resultImc < 39.9)
     {
         showResult = 'Obesidade, grau II';
     }
@@ -30,5 +36,6 @@ function imc()
         showResult = 'Obesidade grave, grau III';
     }
     result.innerText = `Você está na classificação: ${showResult}`;
+    
 }
 calculato.addEventListener('click', imc);
